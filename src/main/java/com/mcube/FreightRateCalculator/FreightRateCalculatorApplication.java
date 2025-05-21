13a0d2a0466a46a13ebe5664bbde18279d56babf
Mirsaid Mirolimjonov <mirsaidmir@gmail.com>
2025-05-21 14:20:48 +0500

    initializing Spring Boot Application

---
+++ src/main/java/com/mcube/FreightRateCalculator/FreightRateCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.mcube.FreightRateCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreightRateCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreightRateCalculatorApplication.class, args);
+	}
+
+}
